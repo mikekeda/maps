3ba491e406124cbc0b6f3d7bad8a648f63ddc766
Mykhailo Keda <mriynuk@gmail.com>
2017-11-01 08:20:48 +0200

    settings.py cleaning.

--- maps/settings.py
+++ maps/settings.py
@@ -35,7 +35,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_var(
@@ -50,7 +49,6 @@ ALLOWED_HOSTS = get_env_var('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -110,8 +108,6 @@ WSGI_APPLICATION = 'maps.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -138,8 +134,6 @@ SESSION_CACHE_ALIAS = "default"
 
 
 # Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -165,8 +159,6 @@ SOCIAL_AUTH_FACEBOOK_KEY = get_env_var('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = get_env_var('SOCIAL_AUTH_FACEBOOK_SECRET')
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 LANGUAGES = [
     ('en', _('English')),
@@ -188,11 +180,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_URL = '/admin/login'
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_ROOT = '/home/voron/sites/cdn/maps'
 
 STATIC_URL = '/static/' if DEBUG else 'https://cdn.mkeda.me/maps/'
