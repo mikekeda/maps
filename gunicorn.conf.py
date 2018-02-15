b59254ffe1b6c0c883901f991f1f0e4931b9f984
Mykhailo Keda <mriynuk@gmail.com>
2018-02-15 22:34:30 +0200

    Installed gunicorn.

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    Gunicorn config.
+"""
+bind = 'unix:/uwsgi/maps.sock'
+workers = 2
+timeout = 30
+max_requests = 100
+daemon = False
+umask = '91'
+user = 'nobody'
+loglevel = 'info'
