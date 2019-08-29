795d89564c92b6ed71433a7a0ea19228245c2295
Mykhailo Keda <mriynuk@gmail.com>
2019-08-29 23:38:07 +0300

    Updated requirements.txt

--- maps/settings.py
+++ maps/settings.py
@@ -6,6 +6,21 @@ import os
 import requests
 from django.utils.translation import ugettext_lazy as _
 
+from django_jenkins.tasks import run_pylint
+
+
+class Lint:
+    """
+    Monkey patch to fix
+    TypeError: __init__() got an unexpected keyword argument 'exit'.
+    """
+    class Run(run_pylint.lint.Run):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, do_exit=kwargs.pop("exit"), **kwargs)
+
+
+run_pylint.lint = Lint
+
 SITE_ENV_PREFIX = 'MAPS'
 
 
