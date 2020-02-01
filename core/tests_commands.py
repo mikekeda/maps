8accff62b7bc05982762a76812f6be56d87aa98a
Mykhailo Keda <mriynuk@gmail.com>
2020-02-01 14:10:26 +0200

    Code cleaning.

--- core/tests_commands.py
+++ core/tests_commands.py
@@ -1,9 +1,9 @@
+from io import StringIO
 import sys
 import types
 
 from django.core.management import call_command
 from django.test import TestCase
-from io import StringIO
 
 from core.models import Polygon
 
--- core/tests_views.py
+++ core/tests_views.py
@@ -1,10 +1,10 @@
+from io import StringIO
 import sys
 
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import TestCase
 from django.urls import reverse
-from io import StringIO
 
 from core.models import Polygon
 from core.views import range_data
