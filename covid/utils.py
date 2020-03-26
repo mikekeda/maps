f2e1895ccb6cf596e4a3a6016aa6950af448e477
Mykhailo Keda <mriynuk@gmail.com>
2020-03-26 16:23:42 +0100

    Fixed covid data parsing.

--- covid/utils.py
+++ covid/utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 import requests
 
@@ -55,6 +56,13 @@ def get_covid_country_data(country):
             'Львівська': 'Lviv Oblast',
             'Тернопільська': 'Ternopil Oblast',
             'Харківська': 'Kharkiv Oblast',
+            'Вінницька': 'Vinnytsia Oblast',
+            'Волинська': 'Volyn Oblast',
+            'Запорізька': 'Zaporizhia Oblast',
+            'Луганська': 'Luhansk Oblast',
+            'Одеська': 'Odessa Oblast',
+            'Херсонська': 'Kherson Oblast',
+            'Черкаська': 'Cherkasy Oblast',
         }
 
         country = Polygon.objects.filter(level=0, title='Ukraine').first()
@@ -68,17 +76,24 @@ def get_covid_country_data(country):
         res = requests.get(url)
         if res.status_code == 200:
             soup = BeautifulSoup(res.content, 'html.parser')
-            for li in soup.select('.medical__vacancy-desc ul:first-of-type > li'):
+            for li in soup.select('.medical__vacancy-desc p'):
                 data = li.text.split()
+                if not data:
+                    continue
+
                 province = data[0]
-                data = [int(node.strip('(').strip('-')) for node in data if node.strip('(').strip('-').isdigit()]
+                data = [
+                    int(node.strip('(').strip('-').strip(';'))
+                    for node in data
+                    if node.strip('(').strip('-').strip(';').isdigit()
+                ]
                 data = [data[0] if data else 0, data[1] if len(data) > 1 else 0, data[2] if len(data) > 2 else 0]
 
                 if province in mapping:
                     stats[mapping[province]] = {
-                        'confirmed': data[0],
-                        'deaths': data[1],
-                        'recovered': data[2],
+                        'confirmed': stats.get(mapping[province], {}).get('confirmed', 0) + data[0],
+                        'deaths': stats.get(mapping[province], {}).get('deaths', 0) + data[1],
+                        'recovered': stats.get(mapping[province], {}).get('recovered', 0) + data[2],
                     }
                 elif province == 'м.':
                     stats['Kiev Oblast'] = {
@@ -90,6 +105,9 @@ def get_covid_country_data(country):
             cache.set(f'covid_19_{country}_last_modified', datetime.now().isoformat(), None)
 
     else:
+        if country == 'United States':
+            country = 'US'
+
         res = requests.get(
             "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats",
             headers={
@@ -101,10 +119,14 @@ def get_covid_country_data(country):
         if res.status_code == 200:
             res = res.json()
 
-            stats = {
-                v['province']: v
-                for v in res['data']['covid19Stats']
-            }
+            stats = defaultdict(lambda: {'confirmed': 0, 'deaths': 0, 'recovered': 0})
+            for v in res['data']['covid19Stats']:
+                stats[v['province']]['confirmed'] += v['confirmed']
+                stats[v['province']]['deaths'] += v['deaths']
+                stats[v['province']]['recovered'] += v['recovered']
+
+            stats = dict(stats)
+
             cache.set(f'covid_19_{country}_last_modified', res['data']['lastChecked'], None)
 
     if stats:
