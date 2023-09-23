4c8eee1463e95365cc424077b80362d0cae6cb2e
Mykhailo Keda <mkeda@Mac-Studio.local>
2023-09-23 10:51:59 +0100

    Added allauth.account.middleware.AccountMiddleware to MIDDLEWARE to fix tests.

--- maps/settings.py
+++ maps/settings.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 if DEBUG:
